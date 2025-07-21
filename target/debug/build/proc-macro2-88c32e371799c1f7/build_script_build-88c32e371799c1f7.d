8b0e212cb46058e2a9f473901f6678667d49b924
Anson Strauch <a1strauch@yahoo.com>
2025-07-21 10:14:28 -0400

    external repo

--- target/debug/build/proc-macro2-88c32e371799c1f7/build_script_build-88c32e371799c1f7.d
+++
@@ -1,5 +0,0 @@
-/home/anson/rust_gravity/target/debug/build/proc-macro2-88c32e371799c1f7/build_script_build-88c32e371799c1f7.d: /home/anson/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.95/build.rs
-
-/home/anson/rust_gravity/target/debug/build/proc-macro2-88c32e371799c1f7/build_script_build-88c32e371799c1f7: /home/anson/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.95/build.rs
-
-/home/anson/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.95/build.rs:
